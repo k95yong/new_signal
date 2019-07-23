e89d1425ba9309e6dbcef73fbdb84fbbeec10041
Ubuntu <ubuntu@ip-172-31-85-76.ec2.internal>
2019-07-23 11:26:04 +0000

    go

---
+++ make_image.py
@@ -0,0 +1,20 @@
+import urllib.request
+import cv2
+import numpy as np
+import os
+
+def create_pos_n_neg():
+    for file_type in ['neg150']:
+
+        for img in os.listdir(file_type):
+
+            if file_type == 'pos':
+                line = file_type + '/' + img + ' 1 0 0 40 107\n'
+                with open('info_r.dat', 'a') as f:
+                    f.write(line)
+            elif file_type == 'neg150':
+                line = file_type + '/' + img + '\n'
+                with open('bg_r.txt', 'a') as f:
+                    f.write(line)
+
+create_pos_n_neg()
