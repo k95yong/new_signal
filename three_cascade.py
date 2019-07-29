3e56158f8f25f8daadda6a6ecce6c197e4ddf5fd
k95yong <37685633+k95yong@users.noreply.github.com>
2019-07-29 20:08:26 +0900

    Add files via upload

--- three_cascade.py
+++ three_cascade.py
@@ -12,7 +12,7 @@ cv2.namedWindow('cascade')
 tl_cascade = cv2.CascadeClassifier("/Users/gim-yongjun/PycharmProjects/OpenCV_Project/new/signalcascade.xml")
 
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 cv2.createTrackbar('w', 'cascade', 0, 1000, nothing)
 cv2.createTrackbar('h', 'cascade', 0, 1000, nothing)
@@ -23,10 +23,10 @@ cv2.setTrackbarPos('h', 'cascade', 8)
 
 low_blue = np.array([100, 70, 30])
 upper_blue = np.array([120, 255, 255])
-# low_red = np.array([163, 80, 80])
-# upper_red = np.array([183, 255, 255])
-# low_green = np.array([58, 80, 80])
-# upper_green = np.array([78, 255, 255])
+low_red = np.array([163, 80, 80])
+upper_red = np.array([183, 255, 255])
+low_green = np.array([58, 80, 80])
+upper_green = np.array([78, 255, 255])
 
 
 while 1:
@@ -36,10 +36,9 @@ while 1:
 
     ret, img = cap.read()
     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     blue_mask = cv2.inRange(img_hsv, low_blue, upper_blue)
-    # red_mask = cv2.inRange(img_hsv, low_red, upper_red)
-    # green_mask = cv2.inRange(img_hsv, low_green, upper_green)
+    red_mask = cv2.inRange(img_hsv, low_red, upper_red)
+    green_mask = cv2.inRange(img_hsv, low_green, upper_green)
 
     # numOfLabels, img_label, stats, centroids = cv.connectedComponentsWithStats(img_mask)
     # kernal = np.ones((11, 11), np.uint8)
@@ -47,24 +46,27 @@ while 1:
     # img_mask = cv2.morphologyEx(img_mask, cv2.MORPH_CLOSE, kernal)
 
     blue_result = cv2.bitwise_and(img, img, mask=blue_mask)
-    # red_result = cv2.bitwise_and(img, img, mask=red_mask)
-    # green_result = cv2.bitwise_and(img, img, mask=green_mask)
+    red_result = cv2.bitwise_and(img, img, mask=red_mask)
+    green_result = cv2.bitwise_and(img, img, mask=green_mask)
 
-
-
-    traffic_light = tl_cascade.detectMultiScale(blue_result, w, h)
-    # tl_g = tl_cascade_g.detectMultiScale(gray, w, h)
-    # tl_r = tl_cascade_r.detectMultiScale(gray, w, h)
+    numOfLabels, img_label, stats, centroids = cv2.connectedComponentsWithStats(red_mask)
+    gray = cv2.cvtColor(blue_result, cv2.COLOR_BGR2GRAY)
+    traffic_light = tl_cascade.detectMultiScale(gray, 4, 8)
     for (x, y, w, h) in traffic_light:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
         cv2.rectangle(blue_result, (x, y), (x + w, y + h), (255, 255, 0), 2)
-    # for (x, y, w, h) in tl_g:
-    #     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    # for (x, y, w, h) in tl_r:
-    #     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        # for idx, centroid in enumerate(centroids):
+        #     if stats[idx][0] == 0 and stats[idx][1] == 0:
+        #         continue
+        #     if np.any(np.isnan(centroid)):
+        #         continue
+        #     x, y, width, height, area = stats[idx]
+        #     centerX, centerY = int(centroid[0]), int(centroid[1])
+        #     if area > 10:
+        #         cv2.circle(img, (centerX, centerY), 10, (0, 0, 255), 10)
 
     cv2.imshow('blue', blue_result)
-    # cv2.imshow('gray', gray)
+    cv2.imshow('gray', gray)
     # cv2.imshow('red', red_result)
     # cv2.imshow('green', green_result)
     cv2.imshow('cascade', img)
