cfaff4aad51440ee9b781fc432998c33bcdb6f0f
Ubuntu <ubuntu@ip-172-31-85-76.ec2.internal>
2019-07-30 08:09:46 +0000

    noa

--- make_image.py
+++ make_image.py
@@ -9,12 +9,12 @@ def create_pos_n_neg():
         for img in os.listdir(file_type):
 
             if file_type == 'pos':
-                line = file_type + '/' + img + ' 1 0 0 40 115\n'
-                with open('info_img3.dat', 'a') as f:
+                line = file_type + '/' + img + ' 1 0 0 50 28\n'
+                with open('info_noa.dat', 'a') as f:
                     f.write(line)
             elif file_type == 'neg150':
                 line = file_type + '/' + img + '\n'
-                with open('bg_img3.txt', 'a') as f:
+                with open('bg_noa.txt', 'a') as f:
                     f.write(line)
 
 create_pos_n_neg()
