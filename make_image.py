bb01ed839c27348eeba6a874aa65af02e5aba99f
root <root@ip-172-31-44-129.ec2.internal>
2019-08-23 10:10:17 +0000

    fs

--- make_image.py
+++ make_image.py
@@ -1,4 +1,4 @@
-import urllib.request
+# import urllib.request
 import cv2
 import numpy as np
 import os
@@ -9,12 +9,12 @@ def create_pos_n_neg():
         for img in os.listdir(file_type):
 
             if file_type == 'pos':
-                line = file_type + '/' + img + ' 1 0 0 107 45\n'
-                with open('info_st.dat', 'a') as f:
+                line = file_type + '/' + img + ' 1 0  090 37\n'
+                with open('info_fs.dat', 'a') as f:
                     f.write(line)
             elif file_type == 'neg150':
                 line = file_type + '/' + img + '\n'
-                with open('bg_st.txt', 'a') as f:
+                with open('bg_fs.txt', 'a') as f:
                     f.write(line)
 
 create_pos_n_neg()
